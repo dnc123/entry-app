705fc8eb6e4e66207333305d33b16bfae8c52407
Donatas <petrdonatas@gmail.com>
2019-07-29 02:13:26 +0300

    Add table, pagination components and button base component

---
+++ front/src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Button v-show='isNotFirstPage' type='button' @click='$emit("previous-page")'>Previous</Button>
+        <span>{{ activePage }} / {{ lastPage }}</span>
+        <Button v-show='isNotLastPage' @click='$emit("next-page")' type='button'>Next</Button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            firstPage: {
+                type: Number,
+                default: () => 0,
+            },
+
+            lastPage: {
+                type: Number,
+                default: () => 0,
+            },
+
+            activePage: {
+                type: Number,
+                default: () => 0,
+            },
+        },
+
+        computed: {
+            isNotFirstPage() {
+                return this.activePage !== this.firstPage;
+            },
+
+            isNotLastPage() {
+                return this.activePage !== this.lastPage;
+            },
+        },
+    }
+</script>
---
+++ front/src/components/Table.vue
@@ -0,0 +1,22 @@
+<template>
+    <table class='Table'>
+        <thead>
+            <slot name='thead'></slot>
+        </thead>
+        <tbody class='Table__Body'>
+            <slot name='tbody'></slot>
+        </tbody>
+    </table>
+</template>
+
+<style lang='scss' scoped>
+    .Table {
+        &__Body {
+            &__Row {
+                &--active {
+                    background-color: green;
+                }
+            }
+        }
+    }
+</style>
